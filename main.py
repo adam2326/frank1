2a66572e2ae0d02dc9fe1453db09dfc9e978f011
adam2326 <adampilz.2326@gmail.com>
2016-11-25 12:47:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def user_page(username):
 	# If user is logged in
 	if user:
 		nickname = users.nickname()
-		login_logout_url = users.create_logout_url('/')
+		login_logout_url = user.create_logout_url('/')
 		# next line is binding. logged in user must match the page they are requesting
 		if username == nickname:
 			# allow them to view the page
